8f36f902d1cb0f3d3e4d4acc1a97475f4f788fb9
Sathya Karthik <sathyakarthik88@gmail.com>
2023-02-23 15:43:38 +0530

    base spring boot rest api project

---
+++ src/main/java/com/example/springbootresetapi/SpringbootresetapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootresetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootresetapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootresetapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootresetapi/SpringbootresetapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootresetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootresetapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
